d44e11a01f409c853c99f654d3a43362af497441
Kavia AI <production@kavia.ai>
2025-12-04 14:06:02 +0000

    Checkpoint 9 - cgad1864e3

--- zoo_monitoring_frontend/src/index.js
+++ zoo_monitoring_frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/theme.css';
+import './styles/layout.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { Toaster } from './components/ui/States';
